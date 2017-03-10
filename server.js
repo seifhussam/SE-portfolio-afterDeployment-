d940023cf941e79ee2ba5d692cc8d6650a1504d5
seifhussamSe <seifel-din.mostafa@student.guc.edu.eg>
2017-03-10 16:13:35 +0200

    my first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded ({extended:true})) ;
 app.use(express.static(__dirname+'/public'));
 app.use('/api',appRoutes) ;
 
-mongoose.connect('mongodb://<seifhussam>:<Seifhussam22>@ds127190.mlab.com:27190/gucportfolio', function (err) {
+mongoose.connect('mongodb://seifhussam:Seifhussam22@ds127190.mlab.com:27190/gucportfolio', function (err) {
   if (err)
   console.log ("not connected : " + err) ;
   else
